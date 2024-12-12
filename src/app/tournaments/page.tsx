e56e3da3d560f74306e1f7a41f4bed5934bd01f4
Tomáš Vojtěch <tomas.vojtech@gmail.com>
2024-12-12 23:47:53 +0100

    routing fix

--- src/app/tournaments/page.tsx
+++ src/app/tournaments/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useCallback } from "react";
 
 import { createTournament } from "@/app/actions/tournament";
 
 export default function Tournaments() {
+  const router = useRouter();
   const handleClick = useCallback(async () => {
     const { id } = await createTournament();
-    redirect(`/tournaments/${id}`);
-  }, []);
+    router.push(`/tournaments/${id}`);
+  }, [router]);
   return <button onClick={handleClick}>Create New Tournament</button>;
 }

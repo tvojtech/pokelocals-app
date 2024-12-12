4be9a029e1a664ab4664efe88adc87c0694c6806
Tomáš Vojtěch <tomas.vojtech@gmail.com>
2024-12-12 23:59:19 +0100

    revert

--- src/app/tournaments/page.tsx
+++ src/app/tournaments/page.tsx
@@ -1,6 +1,15 @@
-import Link from "next/link";
-import { v4 as uuid } from "uuid";
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useCallback } from "react";
+
+import { createTournament } from "@/app/actions/tournament";
 
 export default function Tournaments() {
-  return <Link href={`/tournaments/${uuid()}`}>Create New Tournament</Link>;
+  const router = useRouter();
+  const handleClick = useCallback(async () => {
+    const { id } = await createTournament();
+    router.push(`/tournaments/${id}`);
+  }, [router]);
+  return <button onClick={handleClick}>Create New Tournament</button>;
 }

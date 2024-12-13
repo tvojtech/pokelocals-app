cbc0ee42ca64aa4236f8a7d72d0eab3bda9e59e8
Tomáš Vojtěch <tomas.vojtech@gmail.com>
2024-12-13 10:17:20 +0100

    asda

--- src/app/tournaments/page.tsx
+++ src/app/tournaments/page.tsx
@@ -8,7 +8,9 @@ import { createTournament } from "@/app/actions/tournament";
 export default function Tournaments() {
   const router = useRouter();
   const handleClick = useCallback(async () => {
+    console.time("createTournament");
     const { id } = await createTournament();
+    console.timeEnd("createTournament");
     router.push(`/tournaments/${id}`);
   }, [router]);
   return <button onClick={handleClick}>Create New Tournament</button>;

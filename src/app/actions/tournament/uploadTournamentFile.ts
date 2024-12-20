db2d4306deb4171701f21c0230247a708827f4fb
Tomáš Vojtěch <tomas.vojtech@gmail.com>
2024-12-20 14:21:53 +0100

    fix and refactor tournament file parsing

--- src/app/actions/tournament/uploadTournamentFile.ts
+++ src/app/actions/tournament/uploadTournamentFile.ts
@@ -6,7 +6,7 @@ import { XMLParser } from "fast-xml-parser";
 import fs from "fs";
 import { revalidatePath } from "next/cache";
 
-import { Tournament } from "./types";
+import { Match, Player, Pod, Round, Subgroup, Tournament } from "./types";
 
 export async function uploadTournamentFile(
   formData: FormData,
@@ -72,65 +72,86 @@ function xmlToObject(xmlString: string): Tournament {
       overflowtablestart: Number(jsonData.tournament.data.overflowtablestart),
     },
     timeelapsed: Number(jsonData.tournament.timeelapsed),
-    players: Array.isArray(jsonData.tournament.players.player)
-      ? jsonData.tournament.players.player.map((p: any) => ({
-          userid: p["@_userid"],
-          firstname: p.firstname,
-          lastname: p.lastname,
-          birthdate: p.birthdate,
-          starter: p.starter === "true",
-          order: Number(p.order),
-          seed: Number(p.seed),
-          creationdate: p.creationdate,
-          lastmodifieddate: p.lastmodifieddate,
-        }))
-      : [],
-    pods: Array.isArray(jsonData.tournament.pods.pod)
-      ? jsonData.tournament.pods.pod.map((p: any) => ({
-          category: p["@_category"],
-          stage: p["@_stage"],
-          subgroups: parseSubgroups(p.subgroups),
-          rounds: Array.isArray(p.rounds.round)
-            ? p.rounds.round.map((r: any) => ({
-                number: r["@_number"],
-                type: r["@_type"],
-                stage: r["@_stage"],
-                matches: Array.isArray(r.matches.match)
-                  ? r.matches.match.map((m: any) => {
-                      return {
-                        outcome: m["@_outcome"],
-                        player1: m.player1
-                          ? m.player1["@_userid"]
-                          : m.player["@_userid"],
-                        // in case of BYE player2 is not present
-                        player2: m.player1 ? m.player2["@_userid"] : undefined,
-                        tablenumber: Number(m.tablenumber),
-                      };
-                    })
-                  : [],
-              }))
-            : [],
-        }))
-      : [],
+    players: parsePlayers(jsonData.tournament.players.player),
+    pods: parsePods(jsonData.tournament.pods.pod),
   };
 
   return tournament;
 }
 
+const parsePlayers = (players: any) => {
+  return parseXmlArray<any, Player>(
+    (p: any) => ({
+      userid: p["@_userid"],
+      firstname: p.firstname,
+      lastname: p.lastname,
+      birthdate: p.birthdate,
+      starter: p.starter === "true",
+      order: Number(p.order),
+      seed: Number(p.seed),
+      creationdate: p.creationdate,
+      lastmodifieddate: p.lastmodifieddate,
+    }),
+    players
+  );
+};
+
+const parsePods = (pods: any) => {
+  return parseXmlArray<any, Pod>(
+    (pod: any) => ({
+      category: pod["@_category"],
+      stage: pod["@_stage"],
+      subgroups: parseSubgroups(pod.subgroups.subgroup),
+      rounds: parseRounds(pod.rounds.round),
+    }),
+    pods
+  );
+};
+
 const parseSubgroups = (subgroups: any) => {
-  if (!subgroups || !subgroups.subgroup) {
-    return [];
-  }
-  if (Array.isArray(subgroups.subgroup)) {
-    return subgroups.subgroup.map((sg: any) => ({
+  return parseXmlArray<any, Subgroup>(
+    (sg: any) => ({
       number: sg["@_number"],
       players: sg.players.player.map((p: any) => p["@_userid"]),
-    }));
-  }
-  return [
-    {
-      number: subgroups.subgroup["@_number"],
-      players: subgroups.subgroup.players.player.map((p: any) => p["@_userid"]),
-    },
-  ];
+    }),
+    subgroups
+  );
 };
+
+const parseMatches = (matches: any) => {
+  return parseXmlArray<any, Match>(
+    (m: any) => ({
+      outcome: m["@_outcome"],
+      player1: m.player1 ? m.player1["@_userid"] : m.player["@_userid"],
+      // in case of BYE player2 is not present
+      player2: m.player1 ? m.player2["@_userid"] : undefined,
+      tablenumber: Number(m.tablenumber),
+    }),
+    matches
+  );
+};
+
+const parseRounds = (rounds: any) => {
+  return parseXmlArray<any, Round>(
+    (round: any) => ({
+      number: round["@_number"],
+      type: round["@_type"],
+      stage: round["@_stage"],
+      matches: parseMatches(round.matches.match),
+    }),
+    rounds
+  );
+};
+
+function parseXmlArray<T, ReturnType>(
+  mapper: (data: T) => ReturnType,
+  data?: T
+) {
+  if (!data) {
+    return [];
+  }
+  if (Array.isArray(data)) {
+    return data.map(mapper);
+  }
+  return [mapper(data)];
+}

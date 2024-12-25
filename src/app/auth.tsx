48f58dbd55aa376f662d6b08e2bd09789225d20e
Tomáš Vojtěch <tomas.vojtech@gmail.com>
2024-12-26 00:12:14 +0100

    Supabase db. NextAuth with supabase.

--- src/app/auth.tsx
+++ src/app/auth.tsx
@@ -1,10 +1,22 @@
-// import { EdgeDBAdapter } from '@auth/edgedb-adapter';
 // import { edgeDbClient } from '@tournaments-gg/web/app/db';
+import PostgresAdapter from "@auth/pg-adapter";
 import NextAuth from "next-auth";
 import Discord from "next-auth/providers/discord";
+import { Pool } from "pg";
+
+const pool = new Pool({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+});
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  // adapter: EdgeDBAdapter(edgeDbClient),
+  debug: process.env.NODE_ENV === "development",
+  adapter: PostgresAdapter(pool),
   providers: [
     // Discord({
     //   clientId: process.env.DISCORD_CLIENT_ID!,

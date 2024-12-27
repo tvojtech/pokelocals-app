ccdbc2f657ed79471cb609d68218a068668d4f7d
Tomáš Vojtěch <tomas.vojtech@gmail.com>
2024-12-28 00:47:23 +0100

    Use edgedb as database.

--- src/app/auth.tsx
+++ src/app/auth.tsx
@@ -1,27 +1,14 @@
-// import { edgeDbClient } from '@tournaments-gg/web/app/db';
-import PostgresAdapter from "@auth/pg-adapter";
+import { EdgeDBAdapter } from "@auth/edgedb-adapter";
 import NextAuth from "next-auth";
 import Discord from "next-auth/providers/discord";
-import { Pool } from "pg";
 
-const pool = new Pool({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
-});
+import { edgeDbClient } from "@/app/db";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   debug: process.env.NODE_ENV === "development",
-  adapter: PostgresAdapter(pool),
-  providers: [
-    // Discord({
-    //   clientId: process.env.DISCORD_CLIENT_ID!,
-    //   clientSecret: process.env.DISCORD_CLIENT_SECRET!,
-    // }),
-    Discord,
-  ],
+  adapter: EdgeDBAdapter(edgeDbClient),
+  providers: [Discord],
+  session: {
+    strategy: "database",
+  },
 });
---
+++ src/app/db.ts
@@ -0,0 +1,6 @@
+import { createClient } from "edgedb";
+
+export const edgeDbClient = createClient({
+  instanceName: process.env.EDGEDB_INSTANCE_NAME,
+  secretKey: process.env.EDGEDB_SECRET_KEY,
+});

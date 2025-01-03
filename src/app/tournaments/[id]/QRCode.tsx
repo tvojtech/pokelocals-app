545e535b3d1b353111243b471c480719ed60eab2
Tomáš Vojtěch <tomas.vojtech@gmail.com>
2025-01-03 02:07:12 +0100

    Fix pairings url in qrcode.

--- src/app/tournaments/[id]/QRCode.tsx
+++ src/app/tournaments/[id]/QRCode.tsx
@@ -18,12 +18,11 @@ export const QRCode: React.FC<{ tournamentId: string }> = ({
     return null;
   }
 
-  const myPairingsUrl = `${origin}/tournaments/${tournamentId}/my-pairings`;
   const pairingsUrl = `${origin}/tournaments/${tournamentId}/pairings`;
 
   return (
     <div className="flex flex-col items-center gap-4">
-      <p className="print:block hidden">{myPairingsUrl}</p>
+      <p className="print:block hidden">{pairingsUrl}</p>
       <Link
         href={pairingsUrl}
         target="_blank"
@@ -34,7 +33,7 @@ export const QRCode: React.FC<{ tournamentId: string }> = ({
       >
         Show pairings <SquareArrowOutUpRight size={20} />
       </Link>
-      <QRCodeSVG value={myPairingsUrl} className="w-60 h-60 m-10" />
+      <QRCodeSVG value={pairingsUrl} className="w-60 h-60 m-10" />
     </div>
   );
 };

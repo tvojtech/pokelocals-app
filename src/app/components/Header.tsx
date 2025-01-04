d8f1ea33edadd2cee04d8d0a8ab6f528723adfc4
Tomas Vojtech <tomas.vojtech@gmail.com>
2025-01-04 22:36:36 +0100

    Style mobile login.

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -9,12 +9,27 @@ import React from "react";
 
 import { Logo } from "@/app/components/Logo";
 import { Button, buttonVariants } from "@/components/ui/button";
-import { Sheet, SheetClose, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import { Separator } from "@/components/ui/separator";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import { cn } from "@/lib/utils";
 
-const HeaderLink: React.FC<React.ComponentProps<typeof Link>> = ({ children, className, ...props }) => {
+const HeaderLink: React.FC<React.ComponentProps<typeof Link>> = ({
+  children,
+  className,
+  ...props
+}) => {
   return (
-    <Link className={cn(buttonVariants({ variant: "link" }), className)} {...props}>
+    <Link
+      className={cn(buttonVariants({ variant: "link" }), className)}
+      {...props}
+    >
       {children}
     </Link>
   );
@@ -58,7 +73,11 @@ export default function Header() {
                     <User2 size={18} />
                     {session.user?.email}
                   </HeaderLink>
-                  <Button variant="ghost" onClick={() => signOut()} title="Logout">
+                  <Button
+                    variant="ghost"
+                    onClick={() => signOut()}
+                    title="Logout"
+                  >
                     <LogOut size={18} />
                   </Button>
                 </div>
@@ -66,22 +85,56 @@ export default function Header() {
             </li>
           </ul>
 
-          <Sheet open={isDrawerOpen} onOpenChange={(open) => toggleDrawer(open)}>
+          <Sheet
+            open={isDrawerOpen}
+            onOpenChange={(open) => toggleDrawer(open)}
+          >
             <SheetTrigger className="lg:hidden">
               <Menu />
             </SheetTrigger>
-            <SheetContent side="left" className="p-4" aria-describedby={undefined}>
+            <SheetContent
+              side="left"
+              className="p-4"
+              aria-describedby={undefined}
+            >
               <SheetHeader>
-                <SheetTitle className="pb-4 pl-4 border-b flex justify-between items-center">
+                <SheetTitle className="pb-4 px-2 border-b flex justify-between items-center">
                   <Logo />
                   <SheetClose asChild onClick={() => toggleDrawer(false)}>
-                    <Button variant="link" size="icon">
+                    <div role="button">
                       <X />
-                    </Button>
+                    </div>
                   </SheetClose>
                 </SheetTitle>
               </SheetHeader>
-              <div className="space-y-2 p-4">
+              <div className="space-y-4 p-4 px-2">
+                {session ? (
+                  <div className="space-y-2">
+                    <p className="text-md">{session.user?.email}</p>
+                    <div className="flex justify-end">
+                      <Button
+                        onClick={() => signOut()}
+                        title="Logout"
+                        variant="default"
+                        className="w-full uppercase flex justify-center items-center"
+                      >
+                        Logout
+                        <LogOut />
+                      </Button>
+                    </div>
+                  </div>
+                ) : (
+                  <Button
+                    title="Sign in"
+                    variant="default"
+                    onClick={sidebarButtonClickHandler("/login")}
+                    className="w-full uppercase flex justify-center items-center"
+                  >
+                    <LogIn />
+                    Sign in
+                  </Button>
+                )}
+                <Separator className="-mx-2 w-[calc(100%+1rem)]" />
                 <Button
                   onClick={sidebarButtonClickHandler("/profile")}
                   variant="ghost"
@@ -90,32 +143,7 @@ export default function Header() {
                   <Settings2 />
                   Settings
                 </Button>
-                {!session && (
-                  <Button
-                    onClick={sidebarButtonClickHandler("/login")}
-                    variant="ghost"
-                    className="justify-start w-full"
-                  >
-                    <LogIn />
-                    Login
-                  </Button>
-                )}
               </div>
-              {session && (
-                <div className="absolute bottom-0 right-0 h-32 w-full p-4">
-                  <div className="border rounded-sm border-l-8 h-full p-4">
-                    <div className="px-4 py-2">
-                      <div className="flex items-center gap-2">
-                        <User2 size={18} />
-                        <p className="text-sm">{session.user?.email}</p>
-                        <Button onClick={() => signOut()} title="Logout" variant="link" size="icon">
-                          <LogOut />
-                        </Button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              )}
             </SheetContent>
           </Sheet>
         </div>

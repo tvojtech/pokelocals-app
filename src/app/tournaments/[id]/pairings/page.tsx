a42a3987c730b44080c2b04bf93ac3de202485e3
Tomas Vojtech <tomas.vojtech@gmail.com>
2025-01-05 23:43:35 +0100

    disable caching for pairings page

--- src/app/tournaments/[id]/pairings/page.tsx
+++ src/app/tournaments/[id]/pairings/page.tsx
@@ -3,6 +3,8 @@ import {
   TournamentPairingsPageProps,
 } from '@/app/tournaments/[id]/pairings/TournamentPairingsPage';
 
+export const dynamic = 'force-dynamic';
+
 export default async function TournamentPairings(
   props: TournamentPairingsPageProps
 ) {

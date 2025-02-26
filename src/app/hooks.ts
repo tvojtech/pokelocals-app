990822e05490e42763ad5492c636545512a28d0f
Tomáš Vojtěch <tomas.vojtech@gmail.com>
2025-02-26 22:07:03 +0100

    From arrow function to plain functions.

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,15 +1,15 @@
 import { useIsClient, useLocalStorage } from '@uidotdev/usehooks';
 
-export const useMyPokemonId = () => {
+export function useMyPokemonId() {
   const [myId, setMyId] = useLocalStorage<string | undefined>('myPokemonId');
 
   return { myId, setMyId };
-};
+}
 
-export const useWindowLocation = () => {
+export function useWindowLocation() {
   const isClient = useIsClient();
   if (!isClient) {
     return undefined;
   }
   return window.location;
-};
+}

1dc4003fe3f7c1957d5e616bbf579381a5cfd208
Tomáš Vojtěch <tomas.vojtech@gmail.com>
2025-03-12 21:23:20 +0100

    Alert texts. (#24)
    * empty commit
    
    * New alert text explaining need to re-enter pokemon id.
    
    * Better alert text

--- src/app/tournaments/[id]/pairings/InlinePokemonIdForm.tsx
+++ src/app/tournaments/[id]/pairings/InlinePokemonIdForm.tsx
@@ -11,6 +11,9 @@ import { Input } from '@/components/ui/input';
 export const InlinePokemonIdCheckForm = clientOnlyComponent(() => {
   const { myId, setMyId } = useMyPokemonId();
   const inputRef = useRef<HTMLInputElement>(null);
+  const isNewDomain =
+    window.location.hostname.includes('pokelocals.online') ||
+    window.location.hostname.includes('localhost');
 
   const onSubmit = () => {
     setMyId(inputRef.current?.value);
@@ -24,7 +27,22 @@ export const InlinePokemonIdCheckForm = clientOnlyComponent(() => {
     <form action={onSubmit} className="space-y-2">
       <Alert
         type="info"
-        message="Enter your Pokemon ID to see your pairings."
+        message={
+          <>
+            <p>Enter your Pokemon ID to see your pairings.</p>
+            {isNewDomain && (
+              <>
+                <p>&nbsp;</p>
+                <p>
+                  You may have filled this before, but due to the domain change,
+                  it must be re-entered.
+                </p>
+                <p>&nbsp;</p>
+                <p>We apologize for the inconvenience.</p>
+              </>
+            )}
+          </>
+        }
       />
       <Input
         ref={inputRef}

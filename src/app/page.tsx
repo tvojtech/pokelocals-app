96f253b3e26f5d6558e6de355cd9e63e1765545a
Tomáš Vojtěch <tomas.vojtech@gmail.com>
2025-04-05 00:58:19 +0200

    Add alert for users without organizer role on DashboardPage
    This update introduces an alert on the DashboardPage to inform users that they need to be an organizer to create tournaments. The alert provides guidance on how to request the organizer role based on the user's authentication status.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,45 @@
+import { auth } from '@clerk/nextjs/server';
 import { SquareArrowRight } from 'lucide-react';
 import Link from 'next/link';
 
 import { listTournaments } from '@/app/actions/tournament';
 import { CreateTournamentButton } from '@/app/tournaments/CreateTournamentButton';
+import { Alert } from '@/components/Alert';
 import { RestrictedPage } from '@/components/RestrictedPage';
 import { buttonVariants } from '@/components/ui/button';
 
 export default async function DashboardPage() {
   const tournaments = await listTournaments();
+  const { userId, orgId } = await auth();
+
+  const alertTitle =
+    "To create tournaments in the future, you'll need to be an organizer.";
+  let alertMessage;
+  if (!orgId) {
+    if (!userId) {
+      alertMessage =
+        'To become one, please sign in, go to profile page, and request the organizer role.';
+    } else {
+      alertMessage =
+        'To become one, go to profile page, and request the organizer role.';
+    }
+  }
+
   return (
     <RestrictedPage>
       <div className="space-y-4">
+        {!orgId && (
+          <Alert
+            type="warning"
+            message={
+              <>
+                <h2 className="text-lg font-bold">{alertTitle}</h2>
+                <p>{alertMessage}</p>
+              </>
+            }
+          />
+        )}
+
         <CreateTournamentButton />
         {tournaments?.length ? (
           <div>

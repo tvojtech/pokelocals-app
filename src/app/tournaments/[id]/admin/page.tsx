3f30d7af415bfbb33f7bca78418f4fa285c2fcb8
Tomáš Vojtěch <tomas.vojtech@gmail.com>
2025-04-06 00:54:29 +0200

    Refactor TournamentAdminPage component and remove legacy file
    This update consolidates the TournamentAdminPage functionality into a single file, enhancing the structure by integrating QRCode and FileUpload components. The previous TournamentAdminPage file has been deleted to streamline the codebase.

--- src/app/tournaments/[id]/admin/page.tsx
+++ src/app/tournaments/[id]/admin/page.tsx
@@ -1,10 +1,30 @@
-import {
-  TournamentAdminPage,
-  TournamentAdminPageProps,
-} from '@/app/tournaments/[id]/admin/TournamentAdminPage';
+import { QRCode } from '@/app/tournaments/[id]/admin/QRCode';
+import { FileUpload } from '@/components/FileUpload';
+import { RestrictedPage } from '@/components/RestrictedPage';
 
-export default async function TournamentDetail(
-  props: TournamentAdminPageProps
-) {
-  return <TournamentAdminPage {...props} />;
+export type TournamentAdminPageProps = {
+  params: Promise<{ id: string }>;
+};
+
+export default async function TournamentAdminPage({
+  params,
+}: TournamentAdminPageProps) {
+  const { id } = await params;
+  return (
+    <RestrictedPage>
+      <h1 className="text-3xl font-bold text-center print:hidden">
+        Tournament
+      </h1>
+      <h1 className="text-3xl font-bold text-center hidden print:block">
+        Pairings
+      </h1>
+      <div className="max-w-lg mx-auto mt-8 space-y-10 flex flex-col items-center justify-center print:h-dvh">
+        <QRCode tournamentId={id} />
+
+        <div className="print:hidden">
+          <FileUpload tournamentId={id} />
+        </div>
+      </div>
+    </RestrictedPage>
+  );
 }

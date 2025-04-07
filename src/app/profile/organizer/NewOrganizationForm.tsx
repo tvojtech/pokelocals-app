76b203755a17ceef413cda96d9ef24418cbf6461
Tomáš Vojtěch <tomas.vojtech@gmail.com>
2025-04-08 01:13:38 +0200

    Add @radix-ui/react-popover dependency and refactor NewOrganizationForm for improved user experience
    This commit introduces the @radix-ui/react-popover package to enhance the user interface with popover hints in the NewOrganizationForm. Additionally, the form structure is updated to use FormControl components for better organization and clarity, replacing city and country fields with a single location field, and improving overall layout and accessibility.

--- src/app/profile/organizer/NewOrganizationForm.tsx
+++ src/app/profile/organizer/NewOrganizationForm.tsx
@@ -1,11 +1,13 @@
 'use client';
 
-import { useActionState, useCallback, useState } from 'react';
+import { Info } from 'lucide-react';
+import { useActionState, useCallback, useId, useState } from 'react';
 
 import { createNewOrganizerRequest } from '@/app/actions/waitlist';
 import { Alert } from '@/components/Alert';
 import { LoadingButton } from '@/components/ui/buttons/loading-button';
 import { Input } from '@/components/ui/input';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import {
   Select,
   SelectContent,
@@ -35,38 +37,103 @@ export function NewOrganizationForm() {
 
   return (
     <>
+      <Alert
+        type="info"
+        title="You have no organization"
+        message={
+          <ul className="list-disc pl-4">
+            <li>Please fill in the form below and we&apos;ll create an organization for you or;</li>
+            <li>if your LGS already has an organization, please ask your administrator to assign you to it.</li>
+          </ul>
+        }
+      />
       <h2 className="text-xl font-bold">Become an organizer</h2>
-      <form action={formAction} className="space-y-2">
-        <Input type="text" name="name" placeholder="Organization name" required autoComplete="off" />
-        <Input
-          type="text"
-          name="website"
-          placeholder="Website (ex.: https://www.example.com)"
-          required
-          autoComplete="off"
-        />
-        <Input type="text" name="city" placeholder="City" autoComplete="off" />
-        <Input type="text" name="country" placeholder="Country" autoComplete="off" />
-        <Select name="avg_tournament_size">
-          <SelectTrigger>
-            <SelectValue placeholder="Select average tournament size" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectLabel>Average tournament size</SelectLabel>
-              <SelectItem value="0_20">0 - 20</SelectItem>
-              <SelectItem value="21_40">21 - 40</SelectItem>
-              <SelectItem value="41_60">41 - 60</SelectItem>
-              <SelectItem value="61_100">61 - 100</SelectItem>
-              <SelectItem value="101_200">101 - 200</SelectItem>
-              <SelectItem value="200_">200+</SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-        <LoadingButton isLoading={isPending} type="submit">
-          Submit
-        </LoadingButton>
+      <form action={formAction} className="grid gap-2 gap-y-4 sm:grid-cols-1 md:grid-cols-2">
+        <FormControl label="Organization name" hint="This is the name of your game store." required>
+          {({ id }) => (
+            <Input id={id} type="text" name="name" placeholder="Organization name" required autoComplete="off" />
+          )}
+        </FormControl>
+        <FormControl
+          label="Website"
+          hint={
+            <>
+              <p>This is the website of your game store.</p>
+              <p>If you don&apos;t have one, please send us link to your league on www.pokemon.com.</p>
+              <p>Example: https://www.pokemon.com/us/play-pokemon/pokemon-events/leagues/6237187</p>
+            </>
+          }
+          required>
+          {({ id }) => <Input id={id} type="text" name="website" placeholder="Website" required autoComplete="off" />}
+        </FormControl>
+        <FormControl label="Location" hint="This is the location of your game store. Example: Prague, Czech Republic">
+          {({ id }) => <Input id={id} type="text" name="location" placeholder="Location" autoComplete="off" />}
+        </FormControl>
+        <FormControl
+          label="Average tournament size"
+          hint="This is the average player count in your tournaments."
+          required>
+          {({ id }) => (
+            <Select name="avg_tournament_size" required>
+              <SelectTrigger id={id}>
+                <SelectValue placeholder="Select average tournament size" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Average tournament size</SelectLabel>
+                  <SelectItem value="0_20">0 - 20</SelectItem>
+                  <SelectItem value="21_40">21 - 40</SelectItem>
+                  <SelectItem value="41_60">41 - 60</SelectItem>
+                  <SelectItem value="61_100">61 - 100</SelectItem>
+                  <SelectItem value="101_200">101 - 200</SelectItem>
+                  <SelectItem value="200_">200+</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          )}
+        </FormControl>
+
+        <div className="col-start-1">
+          <LoadingButton isLoading={isPending} type="submit" className="w-fit">
+            Submit
+          </LoadingButton>
+        </div>
       </form>
     </>
+  );
+}
+
+function FormControl({
+  label,
+  hint,
+  required,
+  children,
+}: {
+  label: string;
+  hint: React.ReactNode;
+  required?: boolean;
+  children: ({ id }: { id: string }) => React.ReactNode;
+}) {
+  const id = useId();
+  return (
+    <div className="space-y-1">
+      <div className="flex items-center justify-between px-2">
+        <label htmlFor={id} className="flex items-center gap-2 leading-4">
+          {label}
+          {hint && (
+            <Popover>
+              <PopoverTrigger>
+                <Info className="h-4 w-4" />
+              </PopoverTrigger>
+              <PopoverContent side="top">
+                <p>{hint}</p>
+              </PopoverContent>
+            </Popover>
+          )}
+        </label>
+        <span className="text-sm text-muted-foreground">{required ? 'Required' : 'Optional'}</span>
+      </div>
+      {children({ id })}
+    </div>
   );
 }

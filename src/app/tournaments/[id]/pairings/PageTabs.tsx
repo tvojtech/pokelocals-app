315788edefd40f3c171dcd7878a5c197e6f0fe79
Tomáš Vojtěch <tomas.vojtech@gmail.com>
2025-04-16 00:17:54 +0200

    Refactor PageTabs component to utilize Tabs for navigation, enhancing user experience with icons for each page type; replace ToggleGroup with Tabs and implement state management for active tab selection.

--- src/app/tournaments/[id]/pairings/PageTabs.tsx
+++ src/app/tournaments/[id]/pairings/PageTabs.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import Link from 'next/link';
-import { useParams, usePathname } from 'next/navigation';
+import { Handshake, List, Trophy, UserRound } from 'lucide-react';
+import { useParams, usePathname, useRouter } from 'next/navigation';
+import { useState } from 'react';
 
-import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group';
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 export enum PageTypes {
   my = 'my',
@@ -12,30 +13,39 @@ export enum PageTypes {
   standings = 'standings',
 }
 
-const pageTypeToTextMappping: Record<PageTypes, { title: string; slug: string }> = {
-  [PageTypes.my]: { title: 'My Pairings', slug: 'my' },
-  [PageTypes.all]: { title: 'Pairings', slug: 'all' },
-  [PageTypes.roster]: { title: 'Roster', slug: 'roster' },
-  [PageTypes.standings]: { title: 'Standings', slug: 'standings' },
+const pageTypeToTextMappping: Record<PageTypes, { title: string; slug: PageTypes; icon: React.ReactNode }> = {
+  [PageTypes.my]: { title: 'My Pairings', slug: PageTypes.my, icon: <UserRound className="h-4 w-4" /> },
+  [PageTypes.all]: { title: 'Pairings', slug: PageTypes.all, icon: <Handshake className="h-4 w-4" /> },
+  [PageTypes.roster]: { title: 'Roster', slug: PageTypes.roster, icon: <List className="h-4 w-4" /> },
+  [PageTypes.standings]: { title: 'Standings', slug: PageTypes.standings, icon: <Trophy className="h-4 w-4" /> },
 };
 
 export function PageTabs({ showStandings }: { showStandings: boolean }) {
   const path = usePathname();
   const { id } = useParams<{ id: string }>();
-
-  const pageType = path.split('/').pop();
+  const urlPageType = path.split('/').pop();
+  const [pageType, setPageType] = useState<PageTypes>((urlPageType as PageTypes) || PageTypes.my);
+  const router = useRouter();
 
   return (
-    <ToggleGroup type="single" value={pageType} className="flex-wrap justify-start">
-      {Object.keys(PageTypes)
-        .filter(key => showStandings || key !== PageTypes.standings)
-        .map(key => (
-          <ToggleGroupItem key={key} value={key} className="text-base">
-            <Link href={`/tournaments/${id}/pairings/${key}`} prefetch={false}>
-              {pageTypeToTextMappping[key as PageTypes].title}
-            </Link>
-          </ToggleGroupItem>
-        ))}
-    </ToggleGroup>
+    <Tabs value={pageType}>
+      <TabsList>
+        {Object.keys(PageTypes)
+          .filter(key => showStandings || key !== PageTypes.standings)
+          .map(key => (
+            <TabsTrigger
+              key={key}
+              value={key}
+              onClick={() => {
+                router.push(`/tournaments/${id}/pairings/${key}`);
+                setPageType(key as PageTypes);
+              }}
+              className="flex items-center gap-2">
+              <span>{pageTypeToTextMappping[key as PageTypes].icon}</span>
+              {pageType === key && <span>{pageTypeToTextMappping[key as PageTypes].title}</span>}
+            </TabsTrigger>
+          ))}
+      </TabsList>
+    </Tabs>
   );
 }

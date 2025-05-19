311aff9818d5087aed4c9b61bdba66cd0a7835ff
Tomáš Vojtěch <tomas.vojtech@gmail.com>
2025-05-19 21:58:49 +0200

    Remove feature flag organization-management.

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,16 +1,14 @@
 import { auth } from '@clerk/nextjs/server';
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import { organizationManagementFlag, organizationStatsFlag } from '@/flags';
+import { organizationStatsFlag } from '@/flags';
 
 import OrganizationManagement from './organization/OrganizationManagement';
 import { PlayerProfile } from './PlayerProfile';
 
 export default async function Profile() {
   const { userId } = await auth();
-  const isOrganizationManagementEnabled = await organizationManagementFlag.run({
-    identify: { userId: userId ?? 'anonymous' },
-  });
+
   const isOrganizationStatsEnabled = await organizationStatsFlag.run({
     identify: { userId: userId ?? 'anonymous' },
   });
@@ -19,17 +17,14 @@ export default async function Profile() {
     <Tabs defaultValue="player">
       <TabsList>
         <TabsTrigger value="player">Player profile</TabsTrigger>
-        {isOrganizationManagementEnabled && userId && <TabsTrigger value="organization">Organization</TabsTrigger>}
+        {userId && <TabsTrigger value="organization">Organization</TabsTrigger>}
       </TabsList>
       <TabsContent value="player">
         <PlayerProfile />
       </TabsContent>
-      {isOrganizationManagementEnabled && userId && (
+      {userId && (
         <TabsContent value="organization">
-          <OrganizationManagement
-            isOrganizationManagementEnabled={isOrganizationManagementEnabled}
-            isOrganizationStatsEnabled={isOrganizationStatsEnabled}
-          />
+          <OrganizationManagement isOrganizationStatsEnabled={isOrganizationStatsEnabled} />
         </TabsContent>
       )}
     </Tabs>

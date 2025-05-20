749f6780c1889e6ad2daa6710d902cd753d6f777
Tomáš Vojtěch <tomas.vojtech@gmail.com>
2025-05-20 23:33:10 +0200

    Shorten missing pokemon id alert text. New domain warning no longer in the text.

--- src/app/tournaments/[id]/pairings/InlinePokemonIdForm.tsx
+++ src/app/tournaments/[id]/pairings/InlinePokemonIdForm.tsx
@@ -11,8 +11,6 @@ import { Input } from '@/components/ui/input';
 export const InlinePokemonIdCheckForm = clientOnlyComponent(() => {
   const { myId, setMyId } = useMyPokemonId();
   const inputRef = useRef<HTMLInputElement>(null);
-  const isNewDomain =
-    window.location.hostname.includes('pokelocals.online') || window.location.hostname.includes('localhost');
 
   const onSubmit = () => {
     setMyId(inputRef.current?.value);
@@ -27,14 +25,6 @@ export const InlinePokemonIdCheckForm = clientOnlyComponent(() => {
       <Alert variant="info">
         <AlertDescription>
           <p>Enter your Pokemon ID to see your pairings.</p>
-          {isNewDomain && (
-            <>
-              <p>&nbsp;</p>
-              <p>You may have filled this before, but due to the domain change, it must be re-entered.</p>
-              <p>&nbsp;</p>
-              <p>We apologize for the inconvenience.</p>
-            </>
-          )}
         </AlertDescription>
       </Alert>
       <Input ref={inputRef} type="text" defaultValue={myId} placeholder="Enter your Pokemon ID" />
